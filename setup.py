d35e82bec7d8cf3fa9485833cb47778cb127085c
Leonardo Quixada <lquixada@gmail.com>
2012-05-29 10:42:50 -0300

    Projeto renomeado para Lightplayer

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'lightbox',
-    version = '1.0.0',
-    description = u'App de lightbox de videos ds projetos de Entretenimento'.encode('utf-8'),
+    name = 'lightplayer',
+    version = '0.0.2',
+    description = u'Lightbox de videos ds projetos de Entretenimento'.encode('utf-8'),
     author = 'Entretenimento',
     author_email = 'entretenimento@corp.globo.com',
     url = 'http://ngit.globoi.com/lightbox',
