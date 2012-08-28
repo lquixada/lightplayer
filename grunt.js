a1a7244785e5bed9c0233b260e2db12f61f80329
Leonardo Quixada <lquixada@gmail.com>
2012-08-27 23:30:48 -0300

    Grunt: adicionados comandos grunt test phantom e grunt test browser

--- grunt.js
+++ grunt.js
@@ -1,24 +1,57 @@
 
-module.exports = function(grunt) {
-    var path = 'lightplayer/media/lightplayer';
+module.exports = function ( grunt ) {
+    var path = 'lightplayer/media/lightplayer',
+        connect = require('connect');
 
     grunt.initConfig({
       concat: {
         dist: {
-          src: [path+'/src-*.js'],
+          src: [path+'/src/*.js'],
           dest: path+'/src.js'
         }
       },
 
       lint: {
-        files: [path+'/src-*.js']
+        files: [path+'/src/*.js']
       },
 
       min: {
         dist: {
-          src: [path+'/src-*.js'],
+          src: [path+'/src/*.js'],
           dest: path+'/min.js'
         }
+      },
+
+      server: {
+        debug: true,
+        port: 8088,
+        base: './lightplayer'
       }
     });
+
+  grunt.registerTask('test', 'server');
+
+  grunt.registerTask('phantom', function () {
+    var url = 'http://localhost:8088/media/lightplayer/runner.html',
+        done = this.async();
+
+    grunt.utils.spawn( { cmd : 'phantomjs', args: ['./scripts/jasmine.js', url] }, function (e,result,code) {
+        grunt.log.writeln( e );
+        grunt.log.writeln( result );
+        
+        if ( code ) {
+            grunt.warn("erro", code);
+        }
+
+        done();
+    });
+  });
+
+  grunt.registerTask('browser', function () {
+    var url = 'http://localhost:8088/media/lightplayer/runner.html';
+
+    this.async();
+
+    grunt.utils.spawn( { cmd : 'open', args: [ url ] }, function () {});
+  });
 };
