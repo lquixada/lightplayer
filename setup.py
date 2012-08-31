d08bc9ccb8b9e5a08788fa202bacb9ded8a87204
Leonardo Quixada <lquixada@gmail.com>
2012-08-31 11:20:27 -0300

    Versão atualizada no setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'lightplayer',
-    version = '1.0.5',
+    version = '1.0.6',
     description = u'Lightbox de videos ds projetos de Entretenimento'.encode('utf-8'),
     author = 'Entretenimento',
     author_email = 'entretenimento@corp.globo.com',
