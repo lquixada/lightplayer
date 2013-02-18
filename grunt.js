2ef36e450cdf677a0c87637264fd5f0a7e75e7a9
Leonardo Quixada <lquixada@gmail.com>
2013-02-17 21:51:33 -0300

    grunt tasks better organized.

--- grunt.js
+++ grunt.js
@@ -1,79 +1,109 @@
+var fs = require('fs');
 
 module.exports = function ( grunt ) {
+	var projectName = 'lightplayer';
 
   grunt.initConfig({
+		lint: {
+      files: ['*.src.js', '*.spec.js', 'js/*.src.js', 'specs/*.spec.js']
+    },
+
     concat: {
-      dist: {
-        src: ['js/*.js'],
-        dest: 'build/src.js'
+      js: {
+        src: ['*.src.js', 'js/*.src.js'],
+        dest: 'build/'+projectName+'.src.js'
       },
 
       css: {
-        src: ['css/*.css'],
-        dest: 'build/src.css'
-      }
-    },
-
-    lint: {
-      files: ['js/*.js']
-    },
-
-    csslint: {
-      dist: {
-        src: 'css/*.css',
-        rules: {
-          "import": false,
-          "overqualified-elements": false,
-          "star-property-hack": false,
-          "box-model": false,
-          "adjoining-classes": false
-        }
+        src: ['*.src.css', 'css/*.src.css'],
+        dest: 'build/'+projectName+'.src.css'
       }
     },
 
     min: {
       dist: {
-        src: ['build/src.js'],
-        dest: 'build/min.js'
+        src: ['*.src.js', 'js/*.src.js'],
+        dest: ''+projectName+'.min.js'
+      },
+
+      build: {
+        src: ['*.src.js', 'js/*.src.js'],
+        dest: 'build/'+projectName+'.min.js'
       }
     },
 
-    // Task com problemas!!!
     cssmin: {
       dist: {
-        src: 'css/*.css',
-        dest: 'build/min.css'
+        src: ['*.src.css', 'css/*.src.css'],
+        dest: ''+projectName+'.min.css'
+      },
+
+      build: {
+        src: ['*.src.css', 'css/*.src.css'],
+        dest: 'build/'+projectName+'.min.css'
       }
     },
     
-    imagescopy: {
-      src: 'imgs',
-      dest: 'build'
+    imgs: {
+			build: {
+				src: 'imgs',
+				dest: 'build'
+			}
+    },
+		
+    watch: {
+        files: ['*.js', 'js/*.js', '*.css', 'css/*.css'],
+        tasks: 'reload'
+    },
+
+    reload: {
+        port: 6001,
+        proxy: {
+            host: 'localhost',
+            port: 8088 // should match server.port config
+        }
+    },
+
+    server: {
+      port: 8088,
+      base: '.'
     }
   });
 
   grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-reload');
 
-  /* Aliased just for readability purposes on "build" task */
-  grunt.registerTask('jsmin', 'min');
-  grunt.registerTask('jslint', 'lint');
-  grunt.registerTask('build', 'test jslint concat jsmin imagescopy');
+  // Aliased tasks (for readability purposes on "build" task)
+  grunt.registerTask('o:cssmin', 'cssmin:build');
+  grunt.registerTask('o:jsmin', 'min:build');
+  grunt.registerTask('o:jslint', 'lint');
+  grunt.registerTask('o:imgs', 'imgs');
+  grunt.registerTask('o:min', 'min:dist');
+  grunt.registerTask('o:test', 'test');
 
-  grunt.registerTask('imagescopy', 'Copy images to the build/', function () {
-      var done = this.async(),
-          src = grunt.config('imagescopy.src'),
-          dest = grunt.config('imagescopy.dest');
+  // Batch taks
+  grunt.registerTask('o:build', 'o:test o:jslint o:jsmin o:cssmin o:imgs');
+  grunt.registerTask('o:watch', 'server reload watch');
 
-      grunt.utils.spawn( { cmd: 'cp', args: ['-R', src, dest] }, function ( err, result, code ) {
-          if ( result.stderr ) {
-              grunt.log.writeln( '\n'+result.stderr+'\n' );
-          } else {
-              grunt.log.writeln( 'Image files copied on "'+dest+'/".' );
-          }
-          
-          done( code>0? false: true );
-      } );
-  } );
+
+  grunt.registerMultiTask('imgs', 'Copy images to the build folder', function () {
+      var done = this.async();
+
+      if (fs.existsSync(this.data.src)) {
+          grunt.utils.spawn({ cmd: 'cp', args: ['-R', this.data.src, this.data.dest]}, function (err, result, code) {
+              if ( result.stderr ) {
+                  grunt.log.writeln( '\n'+result.stderr+'\n' );
+              } else {
+                  grunt.log.writeln('Image files copied successfully.');
+              }
+              
+              done(code>0? false: true);
+          } );
+      } else {
+          grunt.log.writeln('Nothing to do.');
+      }
+  });
+
 
   grunt.registerTask('test', 'Run specs using npm test', function () {
       var done = this.async();
